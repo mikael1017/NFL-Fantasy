cdb5b935e5bfbb2446f11af2f70aec119f80e3f0
Jaewoo <jaewoocho1017@gmail.com>
2021-02-04 16:05:37 -0800

    Passsing param of num of player

--- frontend/src/components/DraftPage.js
+++ frontend/src/components/DraftPage.js
@@ -1,14 +1,29 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { Link } from "react-router-dom";
 
+import Button from "@material-ui/core/Button";
+
+import { Link, useParams } from "react-router-dom";
 export default function DraftPage() {
-  // const { numOfPlayer } = useParams();
+  const { NumOfPlayer } = useParams();
+  console.log(NumOfPlayer);
   return (
-    <Grid>
-      <Typography>Welcome to the Mock Draft!</Typography>
+    <Grid className="buttons" container spacing={1}>
+      <Grid item xs={12} align="left">
+        <Button
+          color="secondary"
+          variant="contained"
+          to="/draft"
+          component={Link}
+        >
+          Leave
+        </Button>
+      </Grid>
+
+      <Grid item xs={12} align="center">
+        <Typography className="title">Welcome to the Mock Draft!</Typography>
+      </Grid>
     </Grid>
   );
 }
--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -39,7 +39,7 @@ export default class Homepage extends Component {
           <Route exact path="/" render={this.renderHomePage} />
           <Route path="/draft" component={Draft} />
           <Route path="/list" component={Data} />
-          <Route path="/mockdraft/:numberOfPlayer" children={DraftPage} />
+          <Route path="/mockdraft/:NumOfPlayer" component={DraftPage} />
         </Switch>
       </Router>
     );
