aedb56443d37907d2e075b2c6d39a8be9da4b9a8
Jaewoo <jaewoocho1017@gmail.com>
2021-02-07 14:58:10 -0800

    edited swap method to reordering method

--- frontend/src/components/DataTable.js
+++ frontend/src/components/DataTable.js
@@ -11,11 +11,8 @@ import SaveIcon from "@material-ui/icons/Save";
 import RestoreIcon from "@material-ui/icons/Restore";
 import Button from "@material-ui/core/Button";
 
-function handleReset() {
-  alert("Clicked reset button");
-}
 export default function DataTable({ data, title }) {
-  const [newData, setNewData] = useState(data);
+  const newData = data;
   const columns = useMemo(() => COLUMNS, []);
   const {
     getTableProps,
@@ -31,6 +28,10 @@ export default function DataTable({ data, title }) {
     useSortBy
   );
 
+  function handleReset() {
+    alert("Clicked reset button");
+  }
+
   function handleSave() {
     if (localStorage.getItem(title) != null) {
       localStorage.removeItem(title);
@@ -40,150 +41,168 @@ export default function DataTable({ data, title }) {
     console.dir(localStorage.getItem(title));
   }
 
-  // useEffect(() => {
-  //   localStorage.setItem("table", JSON.stringify(data));
-
-  //   console.dir(localStorage.getItem("table"));
-  // });
-  function swapPosition(newData, before, after) {
-    const row1 = newData[before];
-    newData[before] = newData[after];
-    newData[after] = row1;
+  function reOrder(newData, before, after) {
+    if (before < after) {
+      const original = newData[before];
+      for (let i = before; i < after; i++) {
+        newData[i] = newData[i + 1];
+      }
+      newData[after] = original;
+    } else {
+      //  after > before
+      const original = newData[before];
+      for (let i = after; i > before; i--) {
+        newData[i] = newData[i - 1];
+      }
+      newData[after] = original;
+    }
   }
-  return (
-    <>
-      <div className="title">{title} </div>
 
-      <DragDropContext
-        onDragEnd={(param) => {
-          if (!param.destination) {
-            return;
-          }
-          const srcIdx = param.source.index;
-          const desIdx = param.destination.index;
-          rows.splice(desIdx, 0, rows.splice(srcIdx, 1)[0]);
-          swapPosition(newData, srcIdx, desIdx);
-        }}
-      >
-        <Droppable droppableId="droppable">
-          {(provided, snapshot) => (
-            <TableContainer
-              style={{ maxHeight: 500, maxWidth: 800 }}
-              className="table-container"
-            >
-              <table
-                ref={provided.innerRef}
-                id={"players" + title}
-                {...getTableProps()}
+  function renderSavedTable() {}
+
+  function renderNewDataTable(paramData) {
+    return (
+      <>
+        <div className="title">{title} </div>
+
+        <DragDropContext
+          onDragEnd={(param) => {
+            if (!param.destination) {
+              return;
+            }
+            const srcIdx = param.source.index;
+            const desIdx = param.destination.index;
+            rows.splice(desIdx, 0, rows.splice(srcIdx, 1)[0]);
+            reOrder(newData, srcIdx, desIdx);
+          }}
+        >
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <TableContainer
+                style={{ maxHeight: 500, maxWidth: 800 }}
+                className="table-container"
               >
-                <thead>
-                  {headerGroups.map((headerGroup) => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                      {headerGroup.headers.map((column) => (
-                        <th
-                          {...column.getHeaderProps(
-                            column.getSortByToggleProps()
-                          )}
-                          className={
-                            column.Header == "Name" ? "sticky-column" : "column"
-                          }
-                        >
-                          {column.render("Header")}
-                          <span className="sort-icon">
-                            {column.isSorted ? (
-                              column.isSortedDesc ? (
-                                <SortUpIcon />
-                              ) : (
-                                <SortDownIcon />
-                              )
-                            ) : (
-                              ""
+                <table
+                  ref={provided.innerRef}
+                  id={"players" + title}
+                  {...getTableProps()}
+                >
+                  <thead>
+                    {headerGroups.map((headerGroup) => (
+                      <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                          <th
+                            {...column.getHeaderProps(
+                              column.getSortByToggleProps()
                             )}
-                          </span>
-                          <div className="filter">
-                            {column.canFilter ? column.render("Filter") : null}
-                          </div>
-                        </th>
-                      ))}
-                    </tr>
-                  ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                  {rows.map((row, i) => {
-                    prepareRow(row);
-                    return (
-                      <Draggable
-                        key={row.id}
-                        draggableId={row.id}
-                        index={i}
-                        className="draggableRow"
-                      >
-                        {(provided, snapshot) => (
-                          <tr
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            {...row.getRowProps()}
-                            style={{
-                              ...provided.draggableProps.style,
-                              boxShadow: snapshot.isDragging
-                                ? "0 0 .4rem #666"
-                                : "none",
-                            }}
+                            className={
+                              column.Header == "Name"
+                                ? "sticky-column"
+                                : "column"
+                            }
                           >
-                            {row.cells.map((cell) => {
-                              return (
-                                <td
-                                  className={
-                                    cell.column.Header == "Name"
-                                      ? "sticky-cell"
-                                      : "cells"
-                                  }
-                                  {...cell.getCellProps()}
-                                >
-                                  {cell.render("Cell")}
-                                </td>
-                              );
-                            })}
-                          </tr>
-                        )}
-                      </Draggable>
-                    );
-                  })}
-                  {provided.placeholder}
-                </tbody>
-              </table>
-            </TableContainer>
-          )}
-        </Droppable>
-      </DragDropContext>
-      <div>
-        <ReactToExcel
-          className="export-btn"
-          table={"players" + title}
-          filename={"NFL_Fantasy_Table (" + title + ")"}
-          sheet="sheet 1"
-          buttonText="Download as XLS"
-        />
-        <Button
-          className="save-btn"
-          color="primary"
-          startIcon={<SaveIcon />}
-          variant="contained"
-          onClick={handleSave}
-        >
-          Save
-        </Button>
-        <Button
-          className="reset-btn"
-          color="secondary"
-          startIcon={<RestoreIcon />}
-          variant="contained"
-          onClick={handleReset}
-        >
-          Restore
-        </Button>
-      </div>
-    </>
-  );
+                            {column.render("Header")}
+                            <span className="sort-icon">
+                              {column.isSorted ? (
+                                column.isSortedDesc ? (
+                                  <SortUpIcon />
+                                ) : (
+                                  <SortDownIcon />
+                                )
+                              ) : (
+                                ""
+                              )}
+                            </span>
+                            <div className="filter">
+                              {column.canFilter
+                                ? column.render("Filter")
+                                : null}
+                            </div>
+                          </th>
+                        ))}
+                      </tr>
+                    ))}
+                  </thead>
+                  <tbody {...getTableBodyProps()}>
+                    {rows.map((row, i) => {
+                      prepareRow(row);
+                      return (
+                        <Draggable
+                          key={row.id}
+                          draggableId={row.id}
+                          index={i}
+                          className="draggableRow"
+                        >
+                          {(provided, snapshot) => (
+                            <tr
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              {...row.getRowProps()}
+                              style={{
+                                ...provided.draggableProps.style,
+                                boxShadow: snapshot.isDragging
+                                  ? "0 0 .4rem #666"
+                                  : "none",
+                              }}
+                            >
+                              {row.cells.map((cell) => {
+                                return (
+                                  <td
+                                    className={
+                                      cell.column.Header == "Name"
+                                        ? "sticky-cell"
+                                        : "cells"
+                                    }
+                                    {...cell.getCellProps()}
+                                  >
+                                    {cell.render("Cell")}
+                                  </td>
+                                );
+                              })}
+                            </tr>
+                          )}
+                        </Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </tbody>
+                </table>
+              </TableContainer>
+            )}
+          </Droppable>
+        </DragDropContext>
+        <div>
+          <ReactToExcel
+            className="export-btn"
+            table={"players" + title}
+            filename={"NFL_Fantasy_Table (" + title + ")"}
+            sheet="sheet 1"
+            buttonText="Download as XLS"
+          />
+          <Button
+            className="save-btn"
+            color="primary"
+            startIcon={<SaveIcon />}
+            variant="contained"
+            onClick={handleSave}
+          >
+            Save
+          </Button>
+          <Button
+            className="reset-btn"
+            color="secondary"
+            startIcon={<RestoreIcon />}
+            variant="contained"
+            onClick={handleReset}
+          >
+            Restore
+          </Button>
+        </div>
+      </>
+    );
+  }
+  return localStorage.getItem(title) == null
+    ? renderNewDataTable(data)
+    : renderNewDataTable(newData);
 }
